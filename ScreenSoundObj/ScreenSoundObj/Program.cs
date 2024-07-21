c755e218a74d50e80b3ba1fb15a42d9c273337e2
Ranielli Soares <ranielli.pinho@outlook.com>
2024-07-21 20:52:36 -0300

    Classe e metodo Musicas

--- ScreenSoundObj/ScreenSoundObj/Program.cs
+++ ScreenSoundObj/ScreenSoundObj/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 =new Musica();
+musica1.nome = "Aquarela";
+musica1.artista = "João de Barro";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Roxane";
+musica2.artista = "The Police";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
--- ScreenSoundObj/ScreenSoundObj/obj/Debug/net8.0/ScreenSoundObj.AssemblyInfo.cs
+++ ScreenSoundObj/ScreenSoundObj/obj/Debug/net8.0/ScreenSoundObj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSoundObj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b68c435ef4c7dccb5d46e81241d0bc7707736b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917faf5d64d2a9884a2b409880b36a3b9e348ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSoundObj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSoundObj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
