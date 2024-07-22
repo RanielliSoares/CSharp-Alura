3700d4c888eb8da297df7b31319999b2567c4bea
Ranielli Soares <ranielli.pinho@outlook.com>
2024-07-21 21:09:13 -0300

    Aplicação bancaria

---
+++ Desafios/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,9 @@
+﻿class DadosConta
+{
+    public string titular;
+    public int numeroConta;
+    public int numeroIndicador;
+    public double saldo;
+    public string senha;
+
+}
---
+++ Desafios/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿DadosConta conta1 = new DadosConta();
+conta1.titular = "Maria dos Anjos";
+conta1.numeroConta = 11241;
+conta1.numeroIndicador = 8;
+conta1.senha = "MinhaSenha";
+conta1.saldo = 10.25;
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+█▀▄▀█ █▄█ ▄▄ █▀▄▀█ █▀█ █▄░█ █▀▀ █▄█
+█░▀░█ ░█░ ░░ █░▀░█ █▄█ █░▀█ ██▄ ░█░░");
+
+}
+
+void ExibirMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nSelecione a opção desejada: ");
+    Console.WriteLine("1 - Verificar meu saldo.");
+    Console.WriteLine("2 - Trocar minha senha.");
+    Console.WriteLine("99 - Sair\n");
+
+    int opcaoSelecionada  = int.Parse(Console.ReadLine())!;
+    switch (opcaoSelecionada)
+    {
+        case 1:
+            Console.WriteLine("");
+            break;
+        case 2:
+            Console.WriteLine("");
+            break;
+        case 99:
+            Console.WriteLine("\nAté mais :)");
+            break;
+        default:
+            Console.WriteLine("\nOpção Selecionada inválida!");
+            Thread.Sleep(1500);
+            Console.Clear();
+            ExibirMenu();
+            break;
+    }
+}
+
+ExibirMenu();
